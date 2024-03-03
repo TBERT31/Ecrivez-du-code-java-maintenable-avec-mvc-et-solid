6edc4adec3cccf3f3f8437ac47b592a87c2f1b86
tbert31 <berteau.thomas@hotmail.fr>
2024-03-03 18:50:51 +0100

    fin du jeu de carte

--- cardgame/src/com/openclassrooms/cardgame/Games.java
+++ cardgame/src/com/openclassrooms/cardgame/Games.java
@@ -3,15 +3,35 @@ package com.openclassrooms.cardgame;
 import com.openclassrooms.cardgame.DeckFactory.DeckType;
 import com.openclassrooms.cardgame.controller.GameController;
 import com.openclassrooms.cardgame.games.HighCardGameEvaluator;
+import com.openclassrooms.cardgame.view.GameSwingPassiveView;
 import com.openclassrooms.cardgame.view.GameSwingView;
+import com.openclassrooms.cardgame.view.GameViewables;
 
 public class Games {
 
     public static void main(String args[]) {
+
+        GameViewables views = new GameViewables();
+
         GameSwingView gsv = new GameSwingView();
         gsv.createAndShowGUI();
 
-        GameController gc = new GameController(DeckFactory.makeDeck(DeckType.Normal), gsv, new HighCardGameEvaluator());
+        views.addViewable(gsv);
+
+        for (int i = 0; i < 3; i++){
+            GameSwingPassiveView passiveView = new GameSwingPassiveView();
+            passiveView.createAndShowGUI();
+
+            views.addViewable(passiveView);
+
+            try{
+                Thread.sleep(2500);
+            }catch (InterruptedException e){
+                e.printStackTrace();
+            }
+        }
+
+        GameController gc = new GameController(DeckFactory.makeDeck(DeckType.Normal), views, new HighCardGameEvaluator());
 
         gc.run();
     }
